3f01e45687acc5906984058c3c9d14f2dd85d6fa
uglygus <cooperbattersby@gmail.com>
2025-09-26 07:00:55 -0400

    cp docker configs

--- setup-docker.sh
+++ setup-docker.sh
@@ -6,18 +6,17 @@ sudo apt upgrade -y
 sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 
 
-
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 
-mkdir -p ~/docker/config/{homepage,emby,prowlarr,transmission}
+#mkdir -p ~/docker/config/{homepage,emby,prowlarr,transmission}
 mkdir -p ~/media/{movies,tv,shorts,downloads,incomplete,torrents}
+cp -R configs/docker ~/docker
+
+sudo usermod -aG docker $USER
 
 sudo systemctl enable docker
 sudo systemctl start docker
 sudo systemctl status docker
 
-sudo usermod -aG docker $USER
 
-
-mkdir -p ~/docker
